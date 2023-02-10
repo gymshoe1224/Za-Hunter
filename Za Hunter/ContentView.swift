b3076762f59364c8d4dd8fe000ffc0f222c516ce
Chris Markiewicz <christopher.e.markiewicz@gmail.com>
2023-02-10 09:25:31 -0600

    Initial Commit

---
+++ Za Hunter/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Za Hunter
+//
+//  Created by Chris Markiewicz on 2/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Za Hunter/Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Za_HunterApp.swift
+//  Za Hunter
+//
+//  Created by Chris Markiewicz on 2/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
