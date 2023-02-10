13ca51a5c6114d86d57193daa9e7c89000fb1c78
Chris Markiewicz <christopher.e.markiewicz@gmail.com>
2023-02-10 09:33:37 -0600

    setup initial Map

---
+++ Za Hunter/Location Manager.swift
@@ -0,0 +1,13 @@
+//
+//  Location Manager.swift
+//  Za Hunter
+//
+//  Created by Chris Markiewicz on 2/10/23.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
+    
+}
