a055d05dec697b9f4a1812d949f75d97f5a67d4e
Chris Markiewicz <christopher.e.markiewicz@gmail.com>
2023-02-10 09:53:18 -0600

    added map search feature

--- Za Hunter/Location Manager.swift
+++ Za Hunter/Location Manager.swift
@@ -9,5 +9,11 @@ import Foundation
 import CoreLocation
 
 class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
-    
+    var locationManager = CLLocationManager()
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
 }
